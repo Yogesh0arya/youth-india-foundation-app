1469a8de87ea0af3ac296cff5b2ede0c3187a17e
Yogesh0arya <yogesh084arya@gmail.com>
2022-02-13 20:45:08 +0530

    third

--- components/Volunteer.js
+++ components/Volunteer.js
@@ -9,7 +9,7 @@ function Volunteer() {
                     <h1 className="text-orange-600 text-center text-2xl font-bold sm:text-4xl md:text-6xl mb-4">VOLUNTEER</h1>
                     <h1 className="font-semibold text-lg md:text-2xl text-black mb-2">OUR OUTREACH CAMPAIGN</h1>
                     <p className="text-gray-700 max-w-2xl">
-                        They say helping someone in times of need is God's work and helping without any expectations is the most human thing one can do. So let&apos;s join hands to reach out to those in need. Volunteer for a good future. Be a volunteer!
+                        They say helping someone in times of need is God&apos;s work and helping without any expectations is the most human thing one can do. So let&apos;s join hands to reach out to those in need. Volunteer for a good future. Be a volunteer!
                     </p>
                     <button className="navBtn text-xl px-4 py-2 mt-5">Be A Volunteer</button>
                 </div>
